d7eb4a121ff233abf671d6b0aba89812574dac49
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-22 16:19:41 +0300

    add RowRouter default test

---
+++ RowRouter/src/test/java/com/andpostman/rowrouter/RowRouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andpostman.rowrouter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RowRouterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
