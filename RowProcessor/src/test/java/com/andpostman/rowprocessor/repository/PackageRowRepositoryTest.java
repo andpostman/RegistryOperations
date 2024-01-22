e2d9a4103cdc885c9ea628fa1bece682322ca276
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-22 16:16:34 +0300

    add RowProcessor test of db repository for PackageRow

---
+++ RowProcessor/src/test/java/com/andpostman/rowprocessor/repository/PackageRowRepositoryTest.java
@@ -0,0 +1,63 @@
+package com.andpostman.rowprocessor.repository;
+
+import com.andpostman.rowprocessor.model.PackageRow;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.test.annotation.DirtiesContext;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyInt;
+
+@DirtiesContext
+@DataR2dbcTest
+class PackageRowRepositoryTest {
+
+    @Autowired
+    private PackageRowRepository repository;
+
+    @Test
+    @DisplayName("Проверка существоания репозитория")
+    void testRepositoryExisted() {
+        assertNotNull(repository);
+    }
+
+    @Test
+    @DisplayName("Проверка пустого запроса в БД")
+    void whenPackageRowNotExist_returnEmptyResult(){
+        Mono<PackageRow> aPackage = repository.findById(anyInt());
+        StepVerifier
+                .create(aPackage)
+                .recordWith(ArrayList::new)
+                .thenConsumeWhile(x -> true)
+                .consumeRecordedWith(organizationPriorities -> assertTrue(organizationPriorities.isEmpty()))
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("Проверка занесения пустого значения в БД")
+    void whenTryToSaveInDbEmptyPackageRow_returnError(){
+        PackageRow aPackage = new PackageRow();
+        Mono<PackageRow> aNewPackage = repository.save(aPackage);
+        StepVerifier
+                .create(aNewPackage)
+                .verifyError();
+    }
+
+    @Test
+    @DisplayName("Проверка обновления талицы бд пустыми значениями")
+    void whenTryToUpdate_thenReturnEmptyResult(){
+        Mono<PackageRow> update = repository.update(anyInt(),anyInt());
+        StepVerifier.create(update)
+                .recordWith(ArrayList::new)
+                .thenConsumeWhile(x -> true)
+                .consumeRecordedWith(organizationPriorities -> assertTrue(organizationPriorities.isEmpty()))
+                .verifyComplete();;
+    }
+}
