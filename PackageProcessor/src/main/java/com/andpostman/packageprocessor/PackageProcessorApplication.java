137edfed9e7f5669326ab2dc06b0ec54b9d21955
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-22 13:14:46 +0300

    add PackageProcessor application runner

---
+++ PackageProcessor/src/main/java/com/andpostman/packageprocessor/PackageProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.andpostman.packageprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PackageProcessorApplication.class, args);
+    }
+
+}
