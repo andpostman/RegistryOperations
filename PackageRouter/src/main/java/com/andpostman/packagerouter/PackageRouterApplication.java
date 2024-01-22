e797ee7bb9a19486ad1287cc8a449e3a2c18a7ea
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-22 13:28:38 +0300

    add PackageRouter application runner

---
+++ PackageRouter/src/main/java/com/andpostman/packagerouter/PackageRouterApplication.java
@@ -0,0 +1,13 @@
+package com.andpostman.packagerouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageRouterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PackageRouterApplication.class, args);
+    }
+
+}
