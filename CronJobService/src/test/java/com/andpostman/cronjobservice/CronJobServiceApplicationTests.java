52ec4dbbd22354530a78c7c79f9f79a863cd2787
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-22 15:58:49 +0300

    add CronJobService default test

---
+++ CronJobService/src/test/java/com/andpostman/cronjobservice/CronJobServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andpostman.cronjobservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CronJobServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
