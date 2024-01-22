041b0b1c12b5944e517cd82e2f651b5914cba434
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-22 16:02:34 +0300

    add PackageProcessor default test

---
+++ PackageProcessor/src/test/java/com/andpostman/packageprocessor/PackageProcessorApplicationTests.java
@@ -0,0 +1,14 @@
+package com.andpostman.packageprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackageProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
