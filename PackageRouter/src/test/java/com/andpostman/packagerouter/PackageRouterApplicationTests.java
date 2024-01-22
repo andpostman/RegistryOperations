bb37579362b738f0053c62d02566f14b7656503e
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-22 16:09:06 +0300

    add PackageRouter default test

---
+++ PackageRouter/src/test/java/com/andpostman/packagerouter/PackageRouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andpostman.packagerouter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackageRouterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
