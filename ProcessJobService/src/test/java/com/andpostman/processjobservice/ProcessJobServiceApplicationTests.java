fb9e4da09b4da1fe15f0a2b0fa1f0391197437d3
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-22 16:11:29 +0300

    add PackageRouter default test

---
+++ ProcessJobService/src/test/java/com/andpostman/processjobservice/ProcessJobServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.andpostman.processjobservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessJobServiceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
