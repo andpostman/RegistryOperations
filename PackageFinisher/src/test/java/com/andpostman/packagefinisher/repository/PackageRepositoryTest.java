c0cc9e115f5a9078308e4dc355ca78640b6f4a02
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-22 16:00:09 +0300

    add PackageFinisher test for PackageRepository

---
+++ PackageFinisher/src/test/java/com/andpostman/packagefinisher/repository/PackageRepositoryTest.java
@@ -0,0 +1,64 @@
+package com.andpostman.packagefinisher.repository;
+
+import com.andpostman.packagefinisher.model.Package;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.test.annotation.DirtiesContext;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyInt;
+
+@DataR2dbcTest
+@DirtiesContext
+class PackageRepositoryTest {
+
+    @Autowired
+    private PackageRepository repository;
+
+    @Test
+    @DisplayName("Проверка существоания репозитория")
+    void testRepositoryExisted() {
+        assertNotNull(repository);
+    }
+
+    @Test
+    @DisplayName("Проверка пустого запроса в БД")
+    void whenPackageNotExist_returnEmptyResult(){
+        Mono<Package> aPackage = repository.findById(anyInt());
+        StepVerifier
+                .create(aPackage)
+                .recordWith(ArrayList::new)
+                .thenConsumeWhile(x -> true)
+                .consumeRecordedWith(organizationPriorities -> assertTrue(organizationPriorities.isEmpty()))
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("Проверка занесения пустого значения в БД")
+    void whenTryToSaveInDbPackage_returnError(){
+        Package aPackage = new Package();
+        Mono<Package> aNewPackage = repository.save(aPackage);
+        StepVerifier
+                .create(aNewPackage)
+                .verifyError();
+    }
+
+    @Test
+    @DisplayName("Проверка получения не существующей таблицы в БД")
+    void whenTryToGetFromDbPackage_returnEmptyResult(){
+        Mono<Package> aNewPackage = repository.getPackageById(-1);
+        StepVerifier
+                .create(aNewPackage)
+                .recordWith(ArrayList::new)
+                .thenConsumeWhile(x -> true)
+                .consumeRecordedWith(organizationPriorities -> assertTrue(organizationPriorities.isEmpty()))
+                .verifyComplete();
+    }
+}
