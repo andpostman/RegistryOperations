9370403ccaa9d262d5048e97b0e3359f16eedcd6
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-22 16:12:22 +0300

    add RowProcessor default test

---
+++ RowProcessor/src/test/java/com/andpostman/rowprocessor/RowProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andpostman.rowprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RowProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
