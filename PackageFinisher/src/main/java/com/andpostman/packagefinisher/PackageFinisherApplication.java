54638091dfdc49380e4043b93b0107282a5b9cff
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-22 12:49:27 +0300

    add PackageFinisher application runner

---
+++ PackageFinisher/src/main/java/com/andpostman/packagefinisher/PackageFinisherApplication.java
@@ -0,0 +1,13 @@
+package com.andpostman.packagefinisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageFinisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PackageFinisherApplication.class, args);
+    }
+
+}
