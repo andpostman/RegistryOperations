5c0e7f2c8880d496bbada680bdbb1f0785b2de57
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-22 12:43:57 +0300

    add PackageFinisher row operation type

---
+++ PackageFinisher/src/main/java/com/andpostman/packagefinisher/property/PackageRowOperationType.java
@@ -0,0 +1,15 @@
+package com.andpostman.packagefinisher.property;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum PackageRowOperationType {
+    PROCESSING(1, "Обработка"),
+    DONE(2, "Выполнено"),
+    ERROR(3, "Работник не найден");
+
+    private final int code;
+    private final String description;
+}
