80d3caa4575c1426bfc485db9d7d8d5f763dd953
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-22 15:39:42 +0300

    add RowProcessor db entity PackageRow

---
+++ RowProcessor/src/main/java/com/andpostman/rowprocessor/model/PackageRow.java
@@ -0,0 +1,50 @@
+package com.andpostman.rowprocessor.model;
+
+import lombok.*;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+import java.math.BigDecimal;
+
+@Table(name = "package_row", schema = "bank" )
+@Getter
+@NoArgsConstructor
+@RequiredArgsConstructor
+@EqualsAndHashCode(exclude = "id")
+public class PackageRow {
+
+    @Id
+    @Column("id")
+    private Integer id;
+
+    @Column("req_id")
+    private @NonNull Integer reqId;
+
+    @Column("account")
+    private @NonNull String account;
+
+    @Column("fio")
+    private @NonNull String fio;
+
+    @Column("amount")
+    private @NonNull BigDecimal amount;
+
+    @Column("green_field")
+    private @NonNull boolean greenField;
+
+    @Column("state")
+    private @NonNull int state;
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("reqId", reqId)
+                .append("account", account)
+                .append("fio", fio)
+                .append("amount", amount)
+                .append("greenField", greenField)
+                .append("state", state)
+                .toString();
+    }
+}
