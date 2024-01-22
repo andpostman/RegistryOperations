3916ff0d7379bf8425504f79b1b502c25a49f1fe
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-22 16:01:41 +0300

    add PackageFinisher default test

---
+++ PackageFinisher/src/test/java/com/andpostman/packagefinisher/PackageFinisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andpostman.packagefinisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackageFinisherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
