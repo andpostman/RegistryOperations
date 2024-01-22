dabe595c35e899639bf4a5d95223a6f8c1edf297
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-22 15:32:31 +0300

    add ProcessJobService application runner

---
+++ ProcessJobService/src/main/java/com/andpostman/processjobservice/ProcessJobServiceApplication.java
@@ -0,0 +1,13 @@
+package com.andpostman.processjobservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessJobServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessJobServiceApplication.class, args);
+    }
+
+}
