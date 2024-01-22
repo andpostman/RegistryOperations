b0f8530f20b67a7721b1a8d145f1e7c411ed8132
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-22 15:53:40 +0300

    add RowRouter application runner

---
+++ RowRouter/src/main/java/com/andpostman/rowrouter/RowRouterApplication.java
@@ -0,0 +1,13 @@
+package com.andpostman.rowrouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RowRouterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RowRouterApplication.class, args);
+    }
+
+}
