8b171c8dea57b49c4cc9fe1c744cbe2f0734589f
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-22 15:29:13 +0300

    add ProcessJobService db entity Status

---
+++ ProcessJobService/src/main/java/com/andpostman/processjobservice/model/Status.java
@@ -0,0 +1,38 @@
+package com.andpostman.processjobservice.model;
+
+import lombok.*;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+import java.time.LocalTime;
+
+@Table(name = "check_job", schema = "bank")
+@Getter
+@NoArgsConstructor
+@RequiredArgsConstructor
+@EqualsAndHashCode(exclude = "id")
+public class Status {
+
+    @Id
+    @Column("id")
+    private Integer id;
+
+    @Column("package_id")
+    private @NonNull Integer packageId;
+
+    @Column("insertion_time")
+    private @NonNull LocalTime insertionTime;
+
+    @Column("state")
+    private @NonNull int state;
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("package_id", packageId)
+                .append("insertion_time", insertionTime)
+                .append("state", state)
+                .toString();
+    }
+}
