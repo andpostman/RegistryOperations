0b2b1d7b9857e4eb7972b9e075b7bf9f4340017b
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-22 13:25:00 +0300

    add PackageRouter runtime exception NotFoundDBField

---
+++ PackageRouter/src/main/java/com/andpostman/packagerouter/exception/NotFoundDBFieldException.java
@@ -0,0 +1,7 @@
+package com.andpostman.packagerouter.exception;
+
+public class NotFoundDBFieldException extends RuntimeException{
+    public NotFoundDBFieldException() {
+        super("Not Found");
+    }
+}
