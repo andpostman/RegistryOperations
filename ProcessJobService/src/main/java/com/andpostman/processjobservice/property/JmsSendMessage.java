549c8ff1ce920395c01857dbac6b306d45a718d9
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-22 15:30:05 +0300

    add ProcessJobService artemis producer message

---
+++ ProcessJobService/src/main/java/com/andpostman/processjobservice/property/JmsSendMessage.java
@@ -0,0 +1,13 @@
+package com.andpostman.processjobservice.property;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JmsSendMessage implements Serializable {
+    int packageId;
+}
